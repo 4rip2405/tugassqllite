af7c13e921292dc3634d8ffaeb9a54698df8d973
4rif2405 <arifpaturrahman3@gmail.com>
2023-06-07 23:20:00 +0700

    ngedit models&db

--- lib/models/notes_operationd.dart
+++ lib/models/notes_operationd.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'package:todo_list/dbhelper/DatabaseHelper.dart';
 import 'package:todo_list/models/note.dart';
 
 class NotesOperation with ChangeNotifier {
   List<Note> _notes = [];
   List<Note> get getNotes => _notes;
 
-  void addNewNote(String title, String description) {
-    Note note = Note(title, description);
+  Future<void> addNewNote(String title, String description) async {
+    Note note = Note(
+      title: title,
+      description: description,
+    );
+    int id = await DatabaseHelper.instance.insert(note);
+    note.id = id;
     _notes.add(note);
+    notifyListeners();
+  }
+
+  Future<void> getNotesFromDatabase() async {
+    List<Note> notes = await DatabaseHelper.instance.getNotes();
+    _notes = notes;
     notifyListeners();
   }
 }
